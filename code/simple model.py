39e39f3ecb3651ee3a2610cfa5b6963cd4d3de75
David <d.v.r.chen@student.tue.nl>
2023-05-09 11:04:44 +0200

    changed the initialization of the strategies

--- code/simple model.py
+++ code/simple model.py
@@ -9,26 +9,38 @@ class RandomSearch(SearchStrategy):
     def next_watchpost(self) -> Hideout:
         """Randomly chooses one of the possible hideouts to surveil next.
         """
-       
+
         return random.choice(self.possible_hideouts)
-    
+
 
 class PickOneAndHide(HidingStrategy):
-     def next_hideout(self) -> Hideout:
-        """Return the hideout (determined during the initialization).
+    def next_hideout(self) -> Hideout:
+        """Pick hiding location and always hide there.
         """
 
         try:
             return self.hideout
+
         except AttributeError:
             self.hideout = random.choice(self.possible_hideouts)
             return self.hideout
-    
-
-hideouts = (1, 2, 3, 4, 5)
 
 
-sheriff = RandomSearch()
-outlaw = PickOneAndHide()
-blackwater = Blackwater(sheriff, outlaw, possible_hideouts=hideouts)
-print(blackwater.simulate())
+class RandomHiding(HidingStrategy):
+    def next_hideout(self) -> Hideout:
+        """Return a randomly chosen hideout evey time.
+        """
+
+        return random.choice(self.possible_hideouts)
+
+
+if __name__ == "__main__":
+
+    hideouts = (1, 2, 3, 4, 5)
+
+    sheriff = RandomSearch
+    outlaw = PickOneAndHide
+    blackwater = Blackwater(sheriff, outlaw, possible_hideouts=hideouts)
+
+    print(blackwater.simulate())
+    blackwater.histogram(100_000)
--- code/utils/simple_model.py
+++ code/utils/simple_model.py
@@ -1,28 +1,22 @@
 from abc import ABC, abstractmethod
 from typing import Tuple
+import matplotlib.pyplot as plt
+import numpy as np
 
 Hideout = int
 
 class SearchStrategy(ABC):
-    def __init__(self) -> None:
-        pass
-
-    def set_possible_hideouts(self, possible_hideouts):
+    def __init__(self, possible_hideouts) -> None:
         self.possible_hideouts = possible_hideouts
 
     @abstractmethod
     def next_watchpost(self) -> Hideout:
-        """Return the next house to surveil for the sheriff.
+        """Return the next house to be surveiled by the sheriff.
         """
-        pass
 
 
 class HidingStrategy(ABC):
-    def __init__(self) -> None:
-        pass
-
-
-    def set_possible_hideouts(self, possible_hideouts):
+    def __init__(self, possible_hideouts) -> None:
         self.possible_hideouts = possible_hideouts
 
 
@@ -30,17 +24,14 @@ class HidingStrategy(ABC):
     def next_hideout(self) -> Hideout:
         """Return the next hideout for the outlaw.
         """
-        pass
 
 
 class Blackwater:
-    def __init__(self, sheriff: SearchStrategy, outlaw: HidingStrategy, possible_hideouts: Tuple[Hideout]) -> None:
-        self.sheriff = sheriff
-        self.outlaw = outlaw
+    def __init__(self, sheriff_strategy: SearchStrategy, outlaw_strategy: HidingStrategy, possible_hideouts: Tuple[Hideout]) -> None:
+        self.sheriff = sheriff_strategy(possible_hideouts)
+        self.outlaw = outlaw_strategy(possible_hideouts)
         self.possibel_hideouts = possible_hideouts
 
-        self.sheriff.set_possible_hideouts(possible_hideouts)
-        self.outlaw.set_possible_hideouts(possible_hideouts)
         
     def caught_next_day(self) -> bool:
         """Progress one day,
@@ -51,9 +42,9 @@ class Blackwater:
 
         if hideout == watchpost:
             return True
-        
+
         return False
-        
+
 
     def simulate(self) -> int:
         days = 0
@@ -62,4 +53,21 @@ class Blackwater:
             days += 1
 
         return days
+
+    def histogram(self, n):
+        trials = [self.simulate() for _ in range(n)]
+        bins = range(max(trials) + 1)
+
+        n, bins, patches = plt.hist(x=trials, bins=bins, color='#0504aa',
+                            alpha=0.7, rwidth=0.85)
+        plt.grid(axis='y', alpha=0.75)
+        plt.xlabel('# Days')
+        plt.ylabel('Count')
+        plt.title('title')
+
+        maxfreq = n.max()
+        # Set a clean upper y-axis limit.
+        plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+        
+        plt.show()
 
