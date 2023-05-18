d44d6268ce925385a948e726a64de61b740ac49b
David Chen <nocktavius@gmail.com>
2023-05-18 10:40:09 +0200

    need some code from different computer

--- code/utils/graph_based.py
+++ code/utils/graph_based.py
@@ -11,53 +11,47 @@ Hideout = int
 
 
 class SearchStrategy(ABC):
-    def __init__(self, 
-                 possible_hideouts: nx.DiGraph) -> None:
+    def __init__(self, possible_hideouts: nx.DiGraph) -> None:
         self.possible_hideouts = possible_hideouts
 
     @abstractmethod
     def next_watchpost(self) -> Hideout:
-        """Return the next house to be surveiled by the sheriff.
-        """
-
+        """Return the next house to be surveiled by the sheriff."""
 
     def __repr__(self) -> str:
         return "Base class for the sheriff searching strategy."
 
 
 class HidingStrategy(ABC):
-    def __init__(self, 
-                 possible_hideouts: nx.DiGraph) -> None:
+    def __init__(self, possible_hideouts: nx.DiGraph) -> None:
         self.possible_hideouts = possible_hideouts
-
 
     def __repr__(self) -> str:
         return "Base class for the outlaw hiding strategy."
 
-
     @abstractmethod
     def next_hideout(self) -> Hideout:
-        """Return the next hideout for the outlaw.
-        """
+        """Return the next hideout for the outlaw."""
 
 
 class BlackwaterGraphBased(ABC):
-    def __init__(self, 
-                 sheriff_strategy: SearchStrategy, 
-                 outlaw_strategy: HidingStrategy, 
-                 possible_hideouts: nx.DiGraph) -> None:
+    def __init__(
+        self,
+        sheriff_strategy: SearchStrategy,
+        outlaw_strategy: HidingStrategy,
+        possible_hideouts: nx.DiGraph,
+    ) -> None:
+        """Simulates the inhomgeneous transition probabilities."""
+
         self.sheriff = sheriff_strategy(possible_hideouts)
         self.outlaw = outlaw_strategy(possible_hideouts)
         self.possibel_hideouts = possible_hideouts
 
-
     def __repr__(self) -> str:
         return "Blackwater, a small town in the Wild West."
 
-        
     def caught_next_day(self) -> bool:
-        """Progress one day.
-        """
+        """Progress one day."""
 
         watchpost = self.sheriff.next_watchpost()
         hideout = self.outlaw.next_hideout()
@@ -67,7 +61,6 @@ class BlackwaterGraphBased(ABC):
 
         return False
 
-
     def simulate(self) -> int:
         days = 0
 
@@ -76,21 +69,21 @@ class BlackwaterGraphBased(ABC):
 
         return days
 
-
     def histogram(self, n):
         trials = [self.simulate() for _ in range(n)]
         bins = range(max(trials) + 1)
 
-        n, bins, patches = plt.hist(x=trials, bins=bins, color='#0504aa',
-                            alpha=0.7, rwidth=0.85)
-        plt.grid(axis='y', alpha=0.75)
-        plt.xlabel('# Days')
-        plt.ylabel('Count')
+        n, bins, patches = plt.hist(
+            x=trials, bins=bins, color="#0504aa", alpha=0.7, rwidth=0.85
+        )
+        plt.grid(axis="y", alpha=0.75)
+        plt.xlabel("# Days")
+        plt.ylabel("Count")
         title = f"{self.sheriff.__repr__()} vs {self.outlaw.__repr__()}"
         plt.title(title)
 
         maxfreq = n.max()
         # Set a clean upper y-axis limit.
         plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
-        
+
         plt.show()
